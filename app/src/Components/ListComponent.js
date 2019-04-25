2f0eda86f53e4cfc0a7060d76d1a2d50e5fd0085
agrawl10 <agrawl10@illinois.edu>
2019-04-25 11:25:34 -0500

    Added a skeleton for the list component

---
+++ app/src/Components/ListComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactList from 'react-list';
+
+class MyComponent extends React.Component {
+    state = {
+        accounts: []
+    };
+
+    componentWillMount() {
+        loadAccounts(::this.handleAccounts);
+    }
+
+    handleAccounts(accounts) {
+        this.setState({accounts});
+    }
+
+    renderItem(index, key) {
+        return <div key={key}>{this.state.accounts[index].name}</div>;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Accounts</h1>
+                <div style={{overflow: 'auto', maxHeight: 400}}>
+                    <ReactList
+                        itemRenderer={::this.renderItem}
+                        length={this.state.accounts.length}
+                        type='uniform'
+                    />
+                </div>
+            </div>
+        );
+    }
+}
